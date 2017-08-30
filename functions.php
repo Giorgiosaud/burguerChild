e00b0bbf2589606c1b3f23a2046ba8ff249d5e80
Jorge Saud <jorgelsaud@gmail.com>
2017-08-30 16:04:56 -0400

    fixes

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-// function my_theme_enqueue_styles() {
+function my_theme_enqueue_styles() {
 //     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 //     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 //     wp_enqueue_style( 'child-style',
@@ -7,7 +7,7 @@
 //         array( $parent_style ),
 //         wp_get_theme()->get('Version')
 //     );
-//     wp_enqueue_style('childAssets',get_template_directory_uri().'/assets/css/style.css');
-// }
+    wp_enqueue_style('childAssets',get_template_directory_uri().'/assets/css/style.css');
+}
 
-// add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );

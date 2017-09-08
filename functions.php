609267f4b45d0108287dc1ba91e51ff1f81e1b96
Jorge Saud <jorgelsaud@gmail.com>
2017-09-08 17:17:14 -0400

    fix vc

--- functions.php
+++ functions.php
@@ -11,8 +11,3 @@ function my_theme_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-add_action( 'after_setup_theme', 'your_parent_theme_setup', 9 );
-function your_parent_theme_setup() {    
-	//include_once 'vc_templates/menu/zpProductsShortcode.class.php';
-    // add_filter(the_content, your_content_filter);
-}

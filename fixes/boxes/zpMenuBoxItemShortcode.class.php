0e51d214c45c279ee56ca3fb7d39aa54a4e6512f
Jorge Saud <jorgelsaud@gmail.com>
2017-09-09 12:55:17 -0400

    fix menu price

--- fixes/boxes/zpMenuBoxItemShortcode.class.php
+++ fixes/boxes/zpMenuBoxItemShortcode.class.php
@@ -48,51 +48,60 @@ class zpMenuBoxItemShortcode extends ctShortcode implements ctVisualComposerShor
         $imageHtml = '';
         if ($image != '') {
             $imageHtml = '<a class="pull-left" title="' . $title . '" href="' . $image . '" data-rel="prettyPhoto[' . ctMenuBoxShortcode::$gallery_id . ']">
-	                [rounded_img size="54" src="' . $thumb . '"][/rounded_img]
-	            </a>';
-        }
-
-        $mainContainerAtts = array(
-            'class' => array('media', $class)
-        );
-        // var_dump($price);
-        $html = '
-			<div ' . $this->buildContainerAttributes($mainContainerAtts, $atts) . '>
-	           ' . $imageHtml . '
-	            <div class="media-body">
-		            <span class="title">' . $title . '</span>
-			            <p>' . $content . '</p>
-			            <span class="price">
-			            ' . ((ct_get_option('products_currency_position', 'before_price') == 'before_price' || ct_get_option('products_currency_position', 'before_price')=='' ) ? '<em>' . $currency . '</em>' : $price . $subprice) .
-            ((ct_get_option('products_currency_position', 'before_price') == 'before_price' || ct_get_option('products_currency_position', 'before_price')=='') ? $price . $subprice : '<em>' . $currency . '</em>') . '
-                        </span>
-                        '.$postscript.'
-	            </div>
-      		</div>' . $separator . '
-		';
-
-        return do_shortcode($html);
+            [rounded_img size="54" src="' . $thumb . '"][/rounded_img]
+        </a>';
     }
 
-    /**
+    $mainContainerAtts = array(
+        'class' => array('media', $class)
+        );
+        // var_dump($price);
+    $html = '<div ' . $this->buildContainerAttributes($mainContainerAtts, $atts) . '>';
+    $html .=$imageHtml;
+    $html .='<div class="media-body">';
+    $html .='<span class="title">' . $title . '</span><p>' . $content . '</p>';
+    $html.='<span class="price">';
+
+    if(ct_get_option('products_currency_position', 'before_price') == 'before_price' || ct_get_option('products_currency_position', 'before_price')=='' ){
+        $html.='<em>' . $currency . '</em>';
+    }
+    else{
+        $html.= $price . $subprice;
+    } 
+    if(ct_get_option('products_currency_position', 'before_price') == 'before_price' || ct_get_option('products_currency_position', 'before_price')==''){
+        $html.= $price . $subprice;
+    }
+    else{ 
+        '<em>' . $currency . '</em>';
+    }
+    $html.='</span>';
+    $html.=$postscript;
+    $html.='</div>';
+    $html.='</div>';
+    $html=$separator;
+
+return do_shortcode($html);
+}
+
+/**
      * Returns config
      * @return null
      */
-    public function getAttributes() {
-        return array(
-            'title' => array('label' => __('title', 'ct_theme'), 'default' => '', 'type' => 'input', 'help' => __('Every Menu Box Item should be wrapped in Menu Box element', 'ct_theme')),
-            'image' => array('label' => __("image", 'ct_theme'), 'default' => '', 'type' => 'image', 'help' => __("Image source", 'ct_theme')),
-            'thumb' => array('label' => __("thumbnail", 'ct_theme'), 'default' => '', 'type' => 'image', 'help' => __("Image thumbnail. If empty, image will be used instead.", 'ct_theme')),
-            'content' => array('label' => __('content', 'ct_theme'), 'default' => '', 'type' => 'textarea'),
-            'postscript' => array('label' => __('postscript', 'ct_theme'), 'default' => '', 'type' => 'input'),
-            'price' => array('label' => __('price', 'ct_theme'), 'default' => '', 'type' => 'input', 'example' => '456,50'),
-            'subprice' => array('label' => __('subprice', 'ct_theme'), 'default' => '', 'type' => 'input'),
-            'currency' => array('label' => __('currency', 'ct_theme'), 'default' => ct_get_option('products_index_currency', '$'), 'type' => 'input'),
-            'separator' => array('default' => 'yes', 'type' => 'select', 'options' => array('yes' => 'yes', 'no' => 'no'), 'label' => __('Add separator', 'ct_theme'), 'help' => __("add separator to item?", 'ct_theme')),
-            'class' => array('label' => __("Custom class", 'ct_theme'), 'default' => '', 'type' => 'input', 'help' => __('Adding custom class allows you to set diverse styles in css to the element. Type in name of class, which you defined in css. You can add as much classes as you like.', 'ct_theme')),
+public function getAttributes() {
+    return array(
+        'title' => array('label' => __('title', 'ct_theme'), 'default' => '', 'type' => 'input', 'help' => __('Every Menu Box Item should be wrapped in Menu Box element', 'ct_theme')),
+        'image' => array('label' => __("image", 'ct_theme'), 'default' => '', 'type' => 'image', 'help' => __("Image source", 'ct_theme')),
+        'thumb' => array('label' => __("thumbnail", 'ct_theme'), 'default' => '', 'type' => 'image', 'help' => __("Image thumbnail. If empty, image will be used instead.", 'ct_theme')),
+        'content' => array('label' => __('content', 'ct_theme'), 'default' => '', 'type' => 'textarea'),
+        'postscript' => array('label' => __('postscript', 'ct_theme'), 'default' => '', 'type' => 'input'),
+        'price' => array('label' => __('price', 'ct_theme'), 'default' => '', 'type' => 'input', 'example' => '456,50'),
+        'subprice' => array('label' => __('subprice', 'ct_theme'), 'default' => '', 'type' => 'input'),
+        'currency' => array('label' => __('currency', 'ct_theme'), 'default' => ct_get_option('products_index_currency', '$'), 'type' => 'input'),
+        'separator' => array('default' => 'yes', 'type' => 'select', 'options' => array('yes' => 'yes', 'no' => 'no'), 'label' => __('Add separator', 'ct_theme'), 'help' => __("add separator to item?", 'ct_theme')),
+        'class' => array('label' => __("Custom class", 'ct_theme'), 'default' => '', 'type' => 'input', 'help' => __('Adding custom class allows you to set diverse styles in css to the element. Type in name of class, which you defined in css. You can add as much classes as you like.', 'ct_theme')),
         );
 
-    }
+}
 
 	/**
 	 * Returns additional info about VC
@@ -100,8 +109,8 @@ class zpMenuBoxItemShortcode extends ctShortcode implements ctVisualComposerShor
 	 */
 	public function getVisualComposerInfo() {
 		return new ctVisualComposerInfo( $this, array(
-		    'icon' => 'fa-plus',
-            'description'=> __('Add item to menu box', 'ct_theme'), ) );
+          'icon' => 'fa-plus',
+          'description'=> __('Add item to menu box', 'ct_theme'), ) );
 	}
 }
 
